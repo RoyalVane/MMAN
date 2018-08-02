c3f8c526659b98b808056968d1b4c9988b234ae8
Yawei Luo <royalvane@hust.edu.cn>
2018-08-02 18:07:23 +1000

    Update train_options.py

--- options/train_options.py
+++ options/train_options.py
@@ -1,6 +1,5 @@
 from .base_options import BaseOptions
 
-
 class TrainOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
@@ -20,9 +19,9 @@ class TrainOptions(BaseOptions):
         self.parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
         self.parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
         self.parser.add_argument('--no_lsgan', action='store_true', help='do *not* use least square GAN, if false, use vanilla GAN')
-        self.parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
+        self.parser.add_argument('--lambda_A', type=float, default=50.0, help='weight for cycle loss (A -> B -> A)')
         self.parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
-        self.parser.add_argument('--pool_size', type=int, default=50, help='the size of image buffer that stores previously generated images')
+        self.parser.add_argument('--pool_size', type=int, default=0, help='the size of image buffer that stores previously generated images')
         self.parser.add_argument('--no_html', action='store_true', help='do not save intermediate training results to [opt.checkpoints_dir]/[opt.name]/web/')
         self.parser.add_argument('--lr_policy', type=str, default='lambda', help='learning rate policy: lambda|step|plateau')
         self.parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
